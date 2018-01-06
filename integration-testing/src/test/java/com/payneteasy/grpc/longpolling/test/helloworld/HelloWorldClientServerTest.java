944bdeb48bd47044bb88bf6a24e635be49989b2f
Evgeniy Sinev <es@payneteasy.com>
2018-01-06 19:00:29 +0300

    Added UNARY server + client test

---
+++ integration-testing/src/test/java/com/payneteasy/grpc/longpolling/test/helloworld/HelloWorldClientServerTest.java
@@ -0,0 +1,50 @@
+package com.payneteasy.grpc.longpolling.test.helloworld;
+
+import com.payneteasy.grpc.longpolling.client.LongPollingChannelBuilder;
+import com.payneteasy.grpc.longpolling.server.LongPollingServer;
+import com.payneteasy.grpc.longpolling.server.servlet.LongPollingDispatcherServlet;
+import com.payneteasy.grpc.longpolling.test.util.ServerUtils;
+import io.grpc.ManagedChannel;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.internal.ServerListener;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class HelloWorldClientServerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldClientServerTest.class);
+
+    @Test(timeout = 10_000)
+    public void test() {
+        LongPollingServer pollingServer = ServerUtils.createLongPollingServer(new GreeterImpl());
+        ServerListener serverListener = pollingServer.waitForServerListener();
+
+        HelloWorldServer server = new HelloWorldServer(9096, new LongPollingDispatcherServlet(serverListener));
+        try {
+            server.start();
+
+            ManagedChannel channel = LongPollingChannelBuilder.forTarget("http://localhost:9096/test").build();
+            GreeterGrpc.GreeterBlockingStub service = GreeterGrpc
+                    .newBlockingStub(channel)
+                    .withDeadlineAfter(5, TimeUnit.SECONDS);
+
+            for(int i=0; i<10; i++) {
+                HelloRequest request = HelloRequest.newBuilder().setName("send " + i).build();
+                HelloReply reply = service.sayHello(request);
+                LOG.debug("reply: {}", reply);
+                Assert.assertEquals("Hello send " + i, reply.getMessage());
+            }
+
+        } finally {
+            server.shutdown();
+        }
+
+
+    }
+}
